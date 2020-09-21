6a70863d631f31a38d60cdfe5e465c04e184ad18
KenYonekura <p.p.high.llows@gmail.com>
2020-09-21 09:48:06 +0900

    Initial Commit

---
+++ DiscomfortApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiscomfortApp
+//
+//  Created by 米倉謙 on 2020/09/21.
+//  Copyright © 2020 kenyonekura.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
