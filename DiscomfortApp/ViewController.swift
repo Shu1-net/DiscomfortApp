0a368af59fee9fce3533b4853fd93fb78349b0ed
KenYonekura <p.p.high.llows@gmail.com>
2020-09-21 21:30:30 +0900

    計算式完成

--- DiscomfortApp/ViewController.swift
+++ DiscomfortApp/ViewController.swift
@@ -13,8 +13,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        calcResultLabel.text = String("0")
     }
+    
+//    温度テキストフィールド
+    @IBOutlet var temperatureLabel: UITextField!
+    
+//    湿度テキストフィールド
+    @IBOutlet var HumidityLabel: UITextField!
+    
+//    計算ボタン
+    @IBOutlet var CalcButton: UIButton!
+    
+    @IBOutlet var calcResultLabel: UILabel!
+    
+//    計算結果の初期値
+    var calcresult:Double = 0
 
-
-}
+//    計算ボタンのアクション
+    @IBAction func CalcButton(_ sender: Any) {
+        if let temperature = Double(temperatureLabel.text!), let humidity = Double(HumidityLabel.text!){
+                   //        不快指数＝0.81×気温+0.01×湿度x(0.99×温度－14.3)+46.3
+                   calcresult = 0.81 * temperature + 0.01 * humidity * (0.99 * temperature - 14.3) + 46.3
+                   print(calcresult)
+               }
+        
+        }
+        
+    
+        
+    }
 
