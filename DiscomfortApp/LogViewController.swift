378d34aae14a63a34944536d8dbb06ea6f50455c
KenYonekura <p.p.high.llows@gmail.com>
2020-09-24 09:20:12 +0900

    UserDefaults.standard.object のエラーが未解決状態

--- DiscomfortApp/LogViewController.swift
+++ DiscomfortApp/LogViewController.swift
@@ -8,23 +8,58 @@
 
 import UIKit
 
-class LogViewController: UIViewController {
-
+class LogViewController: UIViewController,UITableViewDelegate,UITableViewDataSource{
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //        ViewControllerのユーザーデフォルトで保存した計算結果のnilチェック
+        //        　nil以外だった場合はresultArray2に保存する
+        if UserDefaults.standard.object(forKey: "calc") != nil {
 
-        // Do any additional setup after loading the view.
+            resultArray2 = UserDefaults.standard.object(forKey: "calc") as![String]
+
+            print(resultArray2)
+
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    var resultArray2:[String] = []
+    
+    
+    @IBOutlet var logTableView: UITableView!
+    
+    //    TableViewのCellの数を指定
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    */
-
+    //    計算結果を取得
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return resultArray2.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = logTableView.dequeueReusableCell(withIdentifier: "cell", for:indexPath)
+        
+        cell.textLabel?.text = resultArray2[indexPath.row]
+        
+        print(resultArray2)
+        
+        return cell
+        
+        
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- DiscomfortApp/ViewController.swift
+++ DiscomfortApp/ViewController.swift
@@ -9,94 +9,101 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        calcResultLabel.text = String("0")
-        
+        calcResultLabel.text = String("")
+        explanation.text = String("")
         
     }
     
-//    温度テキストフィールド
+    //    温度テキストフィールド
     @IBOutlet var temperatureLabel: UITextField!
     
-//    湿度テキストフィールド
+    //    湿度テキストフィールド
     @IBOutlet var HumidityLabel: UITextField!
     
-//    計算ボタン
+    //    計算ボタン
     @IBOutlet var CalcButton: UIButton!
     
-//    計算結果を表示させるLabel
+    //    計算結果を表示させるLabel
     @IBOutlet var calcResultLabel: UILabel!
     
-//    計算結果と同じくイメージを表示するLabel
+    //    計算結果と同じくイメージを表示するLabel
     @IBOutlet var ResultImage: UIImageView!
     
-//    説明Label
+    //    説明Label
     @IBOutlet var explanation: UILabel!
     
     
-//    計算結果の初期値
+    
+    //    計算結果の初期値
     var calcresult:Double = 0
     
-
-//    計算ボタンのアクション
+    var dispImageNum = 0
+    
+    func dispImage() {
+        let imageArray = ["samui","ojiisan","uchiwa","atsui"]
+        let name = imageArray[dispImageNum]
+        let image = UIImage(named: name)
+        ResultImage.image = image
+    }
+    
+    var resultArray = [String]()
+    
+    //    計算ボタンのアクション
     @IBAction func CalcButton(_ sender: Any) {
+        //        formatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "ydMMM", options: 0, locale: Locale(identifier: "ja_JP"))
         if let temperature = Double(temperatureLabel.text!), let humidity = Double(HumidityLabel.text!){
-                   //        不快指数＝0.81×気温+0.01×湿度x(0.99×温度－14.3)+46.3
-                   calcresult = 0.81 * temperature + 0.01 * humidity * (0.99 * temperature - 14.3) + 46.3
-                   print(calcresult)
-            
-               }
-        
+            //        不快指数＝0.81×気温+0.01×湿度x(0.99×温度－14.3)+46.3
+            calcresult = 0.81 * temperature + 0.01 * humidity * (0.99 * temperature - 14.3) + 46.3
+//            print(calcresult)
+        }
         switch calcresult {
         case 0..<55:
-            
-             calcResultLabel.text = String(calcresult)
-             explanation.text = "寒い"
-             
-            
+            calcResultLabel.text = String(calcresult)
+            explanation.text = "寒い"
         case 55..<60:
-            
-             calcResultLabel.text = String(calcresult)
-             explanation.text = "肌寒い"
-            
+            calcResultLabel.text = String(calcresult)
+            explanation.text = "肌寒い"
         case 60..<65:
-            
-             calcResultLabel.text = String(calcresult)
-             explanation.text = "何も感じない"
-            
+            calcResultLabel.text = String(calcresult)
+            explanation.text = "何も感じない"
+            dispImageNum = 1
         case 65..<70:
-            
-             calcResultLabel.text = String(calcresult)
-             explanation.text = "快い"
-            
+            calcResultLabel.text = String(calcresult)
+            explanation.text = "快い"
+            dispImageNum = 1
         case 70..<75:
-            
-             calcResultLabel.text = String(calcresult)
+            calcResultLabel.text = String(calcresult)
             explanation.text = "暑くない"
-            
+            dispImageNum = 1
         case 75..<80:
-            
-             calcResultLabel.text = String(calcresult)
+            calcResultLabel.text = String(calcresult)
             explanation.text = "やや暑い"
-            
+            dispImageNum = 2
         case 80..<85:
-            
-             calcResultLabel.text = String(calcresult)
+            calcResultLabel.text = String(calcresult)
             explanation.text = "暑くて汗がでる"
-            
-        case 85..<1000:
-            
+            dispImageNum = 2
+        case 85..<100:
             calcResultLabel.text = String(calcresult)
             explanation.text = "暑くてたまらない"
-            
+            dispImageNum = 3
         default:
-            
-            calcResultLabel.text = ""
+            calcResultLabel.text = "error"
         }
         
-        }
+        dispImage()
+        //        計算ボタンが押されたら､calcResultLabelの値を配列に追加する｡
+        resultArray.append(calcResultLabel.text!)
+        //        testOK!
+        print(resultArray)
+        //        ユーザーデフォルトに保存する
+        UserDefaults.standard.set(calcresult, forKey: "calc")
     }
+    
+    
+}
 
